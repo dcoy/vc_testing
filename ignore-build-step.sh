fd70fe73739ebcb9d24a71c2214206730d99f5c5
David Coy <10887119+dcoy@users.noreply.github.com>
2025-02-24 18:05:31 -0600

    Simplify logic
    Signed-off-by: David Coy <10887119+dcoy@users.noreply.github.com>

--- ignore-build-step.sh
+++ ignore-build-step.sh
@@ -3,27 +3,26 @@
 echo "VERCEL_ENV: $VERCEL_ENV"
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 echo "VERCEL_GIT_COMMIT_SHA: $VERCEL_GIT_COMMIT_SHA"
-echo "ALLOW_DEPLOY_HOOK: $ALLOW_DEPLOY_HOOK"
 
 # Function to determine if it's a Deploy Hook build
 is_deploy_hook_build() {
     [[ -z "$VERCEL_GIT_COMMIT_SHA" && "$VERCEL_GIT_COMMIT_REF" == "main" ]]
 }
 
-# Check if it's a git push or Deploy Hook
+# Check if it's a Deploy Hook build
 if is_deploy_hook_build; then
     echo "This is a Deploy Hook build"
-    # Only build if ALLOW_DEPLOY_HOOK is true
-    if [[ "$ALLOW_DEPLOY_HOOK" == "true" ]]; then
-        echo "✅ - Deploy Hook build allowed for this environment"
+    # Allow Deploy Hook builds only for preview environment
+    if [[ "$VERCEL_ENV" == "preview" ]]; then
+        echo "✅ - Deploy Hook build allowed for preview environment"
         exit 1
     else
-        echo "🛑 - Deploy Hook build not allowed for this environment"
+        echo "🛑 - Deploy Hook build not allowed for $VERCEL_ENV environment"
         exit 0
     fi
 else
     echo "This is a Git push build"
     # Always allow Git push builds
-    echo "✅ - Git push build allowed"
+    echo "✅ - Git push build allowed for $VERCEL_ENV environment"
     exit 1
 fi
