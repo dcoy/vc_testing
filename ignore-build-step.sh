3de10df6cf8bde6625ad6bf9603b4a659fba8150
David Coy <10887119+dcoy@users.noreply.github.com>
2025-02-25 00:50:47 -0600

    Update ignore-build-step.sh

--- ignore-build-step.sh
+++ ignore-build-step.sh
@@ -3,10 +3,11 @@
 echo "VERCEL_ENV: $VERCEL_ENV"
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 echo "VERCEL_GIT_COMMIT_SHA: $VERCEL_GIT_COMMIT_SHA"
+echo "ALLOW_DEPLOY_HOOK: $ALLOW_DEPLOY_HOOK"
 
 # Function to determine if it's a production Deploy Hook build
 is_deploy_hook_build() {
-    [[ -z "$VERCEL_ENV" == "production" ]]
+    [[ -z "$ALLOW_DEPLOY_HOOK" == "false" ]]
 }
 
 # Check if it's a production Deploy Hook build
@@ -14,8 +15,8 @@ if is_deploy_hook_build; then
     echo "This is a production Deploy Hook build"
     echo "🛑 - Deploy Hook build not allowed for production environment"
     exit 0
-else
-    # This could be a preview Deploy Hook build or any Git push build
-    echo "✅ - Build allowed for $VERCEL_ENV environment"
-    exit 1
 fi
+    # This could be a preview Deploy Hook build or any Git push build
+    # echo "✅ - Build allowed for $VERCEL_ENV environment"
+    # exit 1
+# fi
